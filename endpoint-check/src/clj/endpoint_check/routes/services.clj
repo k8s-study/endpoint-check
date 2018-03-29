69545d4594707f1c6f4cf53326966fe8850be8bd
ted <itstedpark@gmail.com>
2018-03-30 00:04:21 +0900

    add backup

--- endpoint-check/src/clj/endpoint_check/routes/services.clj
+++ endpoint-check/src/clj/endpoint_check/routes/services.clj
@@ -12,47 +12,47 @@
                            :description "Sample Services"}}}}
   ;; ping-test
   (context "/api" []
-    :tags ["ping-test"]
-    (POST "/ping" []
-      :body-params [urls :- [String]]
-      :summary "request urls"
-      (def responseStatus (atom []))
-      (ok (let [url urls
-                futures (doall (map http/get url))]
-            (doseq [resp futures]
-              (swap! responseStatus conj {:url (-> @resp :opts :url) :status (:status @resp)})
-              (println (-> @resp :opts :url) " status: " (:status @resp))) @responseStatus))))
-
+           :tags ["ping-test"]
+           (POST "/ping" []
+                 :body-params [urls :- [String]]
+                 :summary "request urls"
+                 (def responseStatus (atom []))
+                 (ok (let [url urls
+                           futures (doall (map http/get url))]
+                       (doseq [resp futures]
+                         (swap! responseStatus conj {:url (-> @resp :opts :url) :status (:status @resp)})
+                         (println (-> @resp :opts :url) " status: " (:status @resp))) @responseStatus))))
+  
   ;; default test api
   (context "/api" []
-    :tags ["thingie"]
+           :tags ["thingie"]
+           
+           (GET "/plus" []
+                :return       Long
+                :query-params [x :- Long, {y :- Long 1}]
+                :summary      "x+y with query-parameters. y defaults to 1."
+                (ok (+ x y)))
 
-    (GET "/plus" []
-      :return       Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary      "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
+           (POST "/minus" []
+                 :return      Long
+                 :body-params [x :- Long, y :- Long]
+                 :summary     "x-y with body-parameters."
+                 (ok (- x y)))
 
-    (POST "/minus" []
-      :return      Long
-      :body-params [x :- Long, y :- Long]
-      :summary     "x-y with body-parameters."
-      (ok (- x y)))
+           (GET "/times/:x/:y" []
+                :return      Long
+                :path-params [x :- Long, y :- Long]
+                :summary     "x*y with path-parameters"
+                (ok (* x y)))
 
-    (GET "/times/:x/:y" []
-      :return      Long
-      :path-params [x :- Long, y :- Long]
-      :summary     "x*y with path-parameters"
-      (ok (* x y)))
+           (POST "/divide" []
+                 :return      Double
+                 :form-params [x :- Long, y :- Long]
+                 :summary     "x/y with form-parameters"
+                 (ok (/ x y)))
 
-    (POST "/divide" []
-      :return      Double
-      :form-params [x :- Long, y :- Long]
-      :summary     "x/y with form-parameters"
-      (ok (/ x y)))
-
-    (GET "/power" []
-      :return      Long
-      :header-params [x :- Long, y :- Long]
-      :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))))
+           (GET "/power" []
+                :return      Long
+                :header-params [x :- Long, y :- Long]
+                :summary     "x^y with header-parameters"
+                (ok (long (Math/pow x y))))))
